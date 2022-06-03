eb40d1c00e8e27b5d7e4d3fd11c26d7ff5a9757d
armaidalstr <armaidalstr@gmail.com>
2022-06-03 15:59:13 +0700

    Initial Commit

---
+++ afl4amadLia/afl4amadLiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  afl4amadLiaApp.swift
+//  afl4amadLia
+//
+//  Created by MacBook Pro on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct afl4amadLiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
